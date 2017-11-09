d49ff099e91fc01f790167133d93ee82af1f905e
Ashley Williams <ashley666ashley@gmail.com>
2017-11-09 11:40:38 +0100

    fix(test): refactor

--- tests/validate.rs
+++ tests/validate.rs
@@ -4,68 +4,44 @@ use validate_npm_package_name::validate;
 
 #[test]
 fn valid_name() {
-  let pkg_names = vec![
-    "some-package",
-    "example.com",
-    "under_score",
-    "period.js",
-    "123numeric"
-  ];
-  for name in pkg_names {
-    let result = validate(name);
-    assert_eq!(result, true, "{} should be a valid package name", name);
-  }
+  assert_eq!(true, validate("some-package"), "some-package should be a valid package name");
+  assert_eq!(true, validate("example.com"), "example.com should be a valid package name");
+  assert_eq!(true, validate("under_score"), "under_score should be a valid package name");
+  assert_eq!(true, validate("dot.js"), "dot.js should be a valid package name");
+  assert_eq!(true, validate("123numeric"), "123numeric should be a valid package name");
 }
 
 #[test]
 fn name_cannot_have_special_characters() {
-  let pkg_names = vec![
-    "~tilde",
-    "back\\slash",
-    "t'ck",
-    "exclamation!",
-    "(openparens",
-    "closeparens)",
-    "asteri*sk",
-  ];
-  for name in pkg_names {
-    let result = validate(name);
-    assert_eq!(result, false, "package name should not contain special characters");
-  }
+  assert_eq!(false, validate("~tilde"), "package name should not contain special characters");
+  assert_eq!(false, validate("back\\slash"), "package name should not contain special characters");
+  assert_eq!(false, validate("t'ck"), "package name should not contain special characters");
+  assert_eq!(false, validate("exclamation!"), "package name should not contain special characters");
+  assert_eq!(false, validate("(openparens"), "package name should not contain special characters");
+  assert_eq!(false, validate("closeparens)"), "package name should not contain special characters");
+  assert_eq!(false, validate("asteri*sk"), "package name should not contain special characters");
 }
 
 #[test]
 fn name_cannot_start_with_period_or_underscore() {
-  let pkg_names = vec![
-    ".dotstart",
-    "_underscorestart",
-  ];
-  for name in pkg_names {
-    let result = validate(name);
-    assert_eq!(result, false, "package name should not start with a period or underscore");
-  }
+  assert_eq!(false, validate(".dotstart"), "package name should not start with a period or underscore");
+  assert_eq!(false, validate("_underscorestart"), "package name should not start with a period or underscore");
 }
 
 #[test]
 fn name_cannot_have_leading_or_trailing_space() {
-  let pkg_names = vec![
-    " leadingspace",
-    "trailingspace ",
-  ];
-  for name in pkg_names {
-    let result = validate(name);
-    assert_eq!(result, false, "package name should not have leading or trialing space");
-  }
+  assert_eq!(false, validate(" leadingspace"), "package name should not have leading or trialing space");
+  assert_eq!(false, validate("trailingspace "), "package name should not have leading or trialing space");
 }
 
 #[test]
 fn name_cannot_be_on_blacklist() {
-  let pkg_names = vec![
-    "node_modules",
-    "favicon.ico",
-  ];
-  for name in pkg_names {
-    let result = validate(name);
-    assert_eq!(result, false, "package name should not be on blacklist");
-  }
+  assert_eq!(false, validate("node_modules"), "package name should not be on blacklist");
+  assert_eq!(false, validate("favicon.ico"), "package name should not be on blacklist");
+}
+
+#[test]
+fn name_cannot_be_a_builtin_module() {
+  assert_eq!(false, validate("http"), "package name should not be a builtin module");
+  assert_eq!(false, validate("fs"), "package name should not be a builtin module");
 }
